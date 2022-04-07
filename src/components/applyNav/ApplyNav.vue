841c8a56b8cb3e5ba4c26415e965ee0fd299abbb
peiqi-debug <982545036@qq.com>
2022-04-07 17:48:10 +0800

    完成吸顶

---
+++ src/components/applyNav/ApplyNav.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="apply-nav" :class="{ is_fixed: isFixed }">
+    <div class="container">
+      <div class="apply-nav-item">功能介绍</div>
+      <div class="apply-nav-item">应用场景</div>
+      <div class="apply-nav-item">特色服务</div>
+      <div class="apply-nav-item">相关推荐</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueHealthcareApplynav',
+
+  data () {
+    return {
+      isFixed: false
+    }
+  },
+
+  mounted () {
+    window.addEventListener('scroll', this.initHeight)
+  },
+
+  methods: {
+    initHeight () {
+      // 在谷歌中： document.pageYOffset  ===   document.documentElement.scrollTop
+      // 在谷歌中获取滚动条滚动了多少,使用：  document.documentElement.scrollTop
+      // 在IE中获取滚动条滚动了多少,使用：   document.body.scrollTop
+      const scrollTop =
+        document.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop
+      this.isFixed = scrollTop >= 510
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./applyNav.scss";
+</style>
--- src/views/apply/Apply.vue
+++ src/views/apply/Apply.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="apply">
-    <div class="container">
-      <div class="demo">
-        <a href="/#/apply/doc/2">技术文档</a>
+
+    <div class="apply-banner">
+      <div class="container">
+        <div class="demo">
+          <a href="/#/apply/doc/2">技术文档</a>
+        </div>
       </div>
     </div>
+
+    <ApplyNav />
   </div>
 </template>
 
 <script>
+import ApplyNav from '../../components/applyNav/ApplyNav.vue'
 export default {
   name: 'HealthcareApply',
-
+  components: {
+    ApplyNav
+  },
   data () {
     return {}
   },
